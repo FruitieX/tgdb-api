9db0f59c953a7120b7025ddcc834770996aff3a5
Rasmus Eskola <fruitiex@gmail.com>
2015-08-14 22:45:20 +0300

    new updated thegamesdb api library

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# tgdb-api
-A simple wrapper for TheGamesDB API
+tgdb-api
+========
+
+Simple wrapper for thegamesdb's API (see
+http://wiki.thegamesdb.net/index.php?title=API_Introduction)
+
+Original code from https://www.npmjs.com/package/thegamesdb-api, adapted to
+work with newer libraries
+
+
+Installation
+------------
+
+    npm install tgdb-api
+
+Usage
+-----
+
+    var gamesdb = require('tgdb-api')
+
+    // All methods return a callback in this form:
+    var cb = function(err,result)
+    {
+        if (!err)
+        {
+            console.log(JSON.stringify(result,undefined,2));
+        }
+    }
+
+    // Available methods:
+    gamesdb.GetGame({id:number}, cb);                   // Get JSON object describing game by thegamesdb id
+    gamesdb.GetGameList({name:string}, cb)              // Get JSON object listing all games that match 'name'
+    gamesdb.GetPlatform({id:number}, cb);               // Get JSON object describing platform by id
+    gamesdb.GetArt({id:number}, cb);                    // Get JSON object describing available art (covers, screenshots, etc.) by id
+    gamesdb.GetPlatformGames({platformId:number}, cb);  // Get JSON object describing all games on platform by platform Id
+    gamesdb.PlatformGames({platformId:number}, cb);     // Same?
+    gamesdb.Updates({time:number}, cb);                 // Get JSON object describing updates to gamesdb since timestamp
+    gamesdb.GetPlatformList(undefined, cb);             // Get the list of all platforms on gamesdb.
+
+
+Example
+-------
+
+    GetGamesList({name:"Halo" }, cb)...
+
+    {
+       Game:
+       [ { id: 9,
+           GameTitle: 'Halo 2',
+           ReleaseDate: '05/31/2007',
+           Platform: 'PC' },
+         { id: 90,
+           GameTitle: 'Halo 3',
+           ReleaseDate: '09/25/2007',
+           Platform: 'Microsoft Xbox 360' },
+         { id: 3597,
+           GameTitle: 'Halo 4',
+           ReleaseDate: '11/06/2012',
+           Platform: 'Microsoft Xbox 360' },
+        ...
+        ]
+     }
+
+
+Todo/Roadmap
+------------
+
+    * Add test cases
+    * Extend art functionality to stream art instead of just retrieving meta data.
---
+++ index.js
@@ -0,0 +1,85 @@
+/*
+  Simple API wrapper for TheGamesDB
+
+  Original code from https://www.npmjs.com/package/thegamesdb-api,
+  adapted to work with newer libraries
+
+  http://wiki.thegamesdb.net/index.php?title=API_Introduction
+*/
+
+'use strict';
+
+var parseString = require('xml2js').parseString;
+var request = require('request');
+
+var baseUrl = 'http://thegamesdb.net/api/';
+
+var methods = [
+  {name:'GetGame',            param:'id',       validator: parseInt},
+  {name:'GetGamesList',       param:'name'},
+  {name:'GetPlatform',        param:'id',       validator: parseInt},
+  {name:'GetArt',             param:'id',       validator: parseInt},
+  {name:'GetPlatformGames',   param:'platform', validator: parseInt},
+  {name:'PlatformGames',      param:'platform', validator: parseInt},
+  {name:'Updates',            param:'time',     validator: parseInt},
+  {name:'GetPlatformsList'}
+];
+
+var requestWrapper = function (method, param, args, cb) {
+  var requestString = baseUrl + method;
+
+  if (param !== undefined && args[param] !== undefined) {
+    requestString = requestString + '?' + param + '=' + args[param];
+  }
+
+  request(requestString, function(err, response, body) {
+    if (!err) {
+      if (response.statusCode == 200) {
+        parseString(body, function(err, res) {
+          if (err || !res) {
+            cb(err, {});
+          } else {
+            cb(err, res.Data);
+          }
+        });
+      } else {
+        cb(new Error('Non 200 Response:' + response.statusCode));
+      }
+    } else {
+      cb(err);
+    }
+  });
+};
+
+var createExport = function(name, param, validator) {
+  module.exports[name] =  function(args, done) {
+    if (param !== undefined) {
+      if (args[param] === undefined) {
+        done(new Error(name + '(): Parameter missing:' + param));
+        return;
+      }
+      if (validator !== undefined && typeof(validator) === 'function') {
+        if (!validator(args[param])) {
+          done(new Error(name + '():' + param  + ' parameter validation failed. Invalid value: ' + args[param]));
+          return;
+        }
+      }
+    }
+
+    requestWrapper(name + '.php', param, args, function(err, res) {
+      if (err) {
+        done(err);
+      }
+      else {
+        done(undefined, res);
+      }
+    });
+  }
+};
+
+// loop through all available methods
+for (var i = 0; i < methods.length; i++) {
+  var method = methods[i];
+
+  createExport(method.name, method.param, method.validator);
+}
